b31665873fe31b3b66d61dc601471aabbbf66396
Kostiantyn <velykorodny@seznam.cz>
2019-01-12 12:17:07 +0100

    book class done

---
+++ Library/src/data_handling/Book.java
@@ -0,0 +1,49 @@
+package data_handling;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BOOKS")
+public class Book {
+	@Id @GeneratedValue
+	@Column(name="ident")
+	private int ident;
+	@Column(name = "title")
+	private String title;
+	@Column(name = "author")
+	private String author;
+	@Column(name = "borrower")
+	private Student borrower; 
+	
+	public int getIdent() {
+			return ident;
+	}
+	public void setIdent(int ident) {
+		this.ident = ident;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public Student getBorrower() {
+		return borrower;
+	}
+	public void setBorrower(Student borrower) {
+		this.borrower = borrower;
+	}	
+	
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+}
---
+++ Library/src/data_handling/Student.java
@@ -0,0 +1,5 @@
+package data_handling;
+
+public class Student {
+
+}
