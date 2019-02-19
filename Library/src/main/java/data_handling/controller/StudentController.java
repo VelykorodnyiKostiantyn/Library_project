2c558ed9bd3761e2979371744fd8daaf2f8d88d5
ga2veln <ga2veln@AP711384.ztb.icb.commerzbank.com>
2019-02-19 08:10:07 +0100

    mvc working config
    When applied, this commit changes url resolving from jsp page to MVC
    servlet

--- Library/src/main/java/data_handling/controller/StudentController.java
+++ Library/src/main/java/data_handling/controller/StudentController.java
@@ -13,64 +13,29 @@ import org.springframework.web.servlet.ModelAndView;
 import data_handling.model.Student;
 import data_handling.service.StudentManager;
 
-
-
-
-
 @Controller
 public class StudentController {
 	@Autowired
 	private StudentManager studentManager;
-	
-    @RequestMapping(value = "/addStudent", method = RequestMethod.GET)
-  public ModelAndView addStudentForm() {
-    	System.out.println("resolve addStudent");
-      return new ModelAndView("addStudentForm", "student", new Student());
-  }
-	
-    @RequestMapping(value = "/addStudentAction", method = RequestMethod.POST)   
-  public String submit(@Validated @ModelAttribute("student")Student student, 
-    BindingResult result, ModelMap model) {
-    	System.out.println("resolve addStudentAction");
-      if (result.hasErrors()) {
-          return "inputError";
-      }
-      studentManager.addStudent(student);
-      model.addAttribute("firstName", student.getFirstName());
-      model.addAttribute("lastName", student.getLastName());
-      model.addAttribute("ident", student.getIdent());
-      model.addAttribute("email", student.getEmail());
-      return "addStudentResult";
-  }
+
+	@RequestMapping(value = "/addStudent", method = RequestMethod.GET)
+	public ModelAndView addStudentForm() {
+		System.out.println("resolve addStudent");
+		return new ModelAndView("addStudentForm", "student", new Student());
+	}
+
+	@RequestMapping(value = "/addStudentAction", method = RequestMethod.POST)
+	public String submit(@Validated @ModelAttribute("student") Student student, BindingResult result, ModelMap model) {
+		System.out.println("resolve addStudentAction");
+		if (result.hasErrors()) {
+			return "inputError";
+		}
+		studentManager.addStudent(student);
+		model.addAttribute("firstName", student.getFirstName());
+		model.addAttribute("lastName", student.getLastName());
+		model.addAttribute("ident", student.getIdent());
+		model.addAttribute("email", student.getEmail());
+		return "addStudentResult";
+	}
 
 }
-
-
-
-
-//
-//import com.commerzbank.ga2veln.springMvc.model.Employee;
-//
-//@Controller
-//public class EmployeeController {
-// 
-//    @RequestMapping(value = "/employee", method = RequestMethod.GET)
-//    public ModelAndView showForm() {
-//    	System.out.print("form");
-//        return new ModelAndView("form", "employee", new Employee());
-//    }
-//
-//     @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)   
-//    public String submit(@Validated @ModelAttribute("employee")Employee employee, 
-//      BindingResult result, ModelMap model) {
-//    	    System.out.print("Start_Mapping");
-//        if (result.hasErrors()) {
-//            return "error";
-//        }
-//        model.addAttribute("name", employee.getName());
-//        model.addAttribute("contactNumber", employee.getContactNumber());
-//        model.addAttribute("id", employee.getId());
-//        System.out.print("FU");
-//        return "employeeView";
-//    }
-//}
